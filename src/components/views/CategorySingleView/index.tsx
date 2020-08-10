21be991a930f7d3fac15c9957875705e74dc42dd
daliborjankov <daliborjankov89@gmail.com>
2020-08-11 01:10:43 +0200

    SingleNewsView language change disabled. Small improvments added.

--- src/components/views/CategorySingleView/index.tsx
+++ src/components/views/CategorySingleView/index.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import { isFailed, isLoaded, isLoading } from "../../../core/common/model";
+import { useHeadlinesPerCategory } from "../../../core/hooks";
+import i18n, { defaultLanguage } from "../../../core/i18next";
+import { isAllowedLanguage, useDeepTranslation } from "../../../utils/helper";
+import { ArticlesNotFound } from "../../common/ArticlesNotFound";
+import { Loader } from "../../common/Loader";
+import { NewsThumbnailList } from "../../common/NewsThumbnailList/NewsThumbnailList";
+import { ViewTemplate } from "../../common/ViewTemplate";
 
 export const CategorySingleView = () => {
-  return <div className="CategorySingleView">CategorySingleView</div>;
+  const { category } = useParams();
+  const { t } = useDeepTranslation();
+
+  const language = isAllowedLanguage(i18n.language) ? i18n.language : defaultLanguage;
+
+  const articles = useHeadlinesPerCategory(language, category);
+
+  return (
+    <ViewTemplate
+      title={`${t(`CategorySingleView.title`, {
+        category: `${t(`CategoriesView.${category}`)}`,
+      })} ${t(`Language.${i18n.language}`)}:`}
+    >
+      {isFailed(articles) && (
+        <ArticlesNotFound
+          text={t(`CategoriesView.error-message`, {
+            category: `${t(`CategoriesView.${category}`)}`,
+          })}
+        />
+      )}
+      {isLoaded(articles) ? (
+        <NewsThumbnailList {...{ articles: articles.value }} />
+      ) : (
+        isLoading(articles) && <Loader />
+      )}
+    </ViewTemplate>
+  );
 };
